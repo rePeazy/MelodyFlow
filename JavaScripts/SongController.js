8e86f7fd4fab8057d950b6f9d858eab206505c0b
Isaac Smets <smetsisaac@gmail.com>
2022-10-21 23:38:51 +0200

    modified:   JavaScripts/SongController.js 	modified:   index.html

--- JavaScripts/SongController.js
+++ JavaScripts/SongController.js
@@ -1,6 +1,16 @@
 var PPButton = document.getElementById("PPButton");
 var testSongButton = document.getElementById("testSongButton");
+
 var audio = document.getElementById("audio");
+var songTitle = document.getAnimations("songName");
+
+audio.onload = function() {
+    audio.files.getMetaData(gotMetaData);
+}
+
+function gotMetaData(metaData) {
+    console.log(metaData);
+}
 
 PPButton.onclick = function() {
     if (audio.paused) {
